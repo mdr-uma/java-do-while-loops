5abe5d0fceafb902668dfc1cbee3707f52abdaad
github username <um_2003_8@hotmail.com>
2022-06-21 16:27:28 -0400

    create a do while loop

--- java-do-while-loops/src/App.java
+++ java-do-while-loops/src/App.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
 
 public class App {
 
 	public static void main(String[] args) {
-
+		Scanner input = new Scanner(System.in);
+		int number = 0;
+		
+		do {
+			System.out.println("Enter a number; ");
+			number = input.nextInt();
+		}
+		while(number != 10);
+		System.out.println("You got 10!");
 	}
 
 }
